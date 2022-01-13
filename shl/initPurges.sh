b67d506ecbefb25607ebd6d8ddf6598a310d95ef
Sari <oukalisalim@outlook.com>
2022-01-13 14:08:42 +0000

    V0

---
+++ shl/initPurges.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#HORODATAGE : Savoir la date et heure du lancement du programme
+HORODATAGE=$(date '+%Y%m%d%H%M')
+#Variables de travail
+DIR_SHL="$PWD"
+DIR_HOME="${DIR_SHL%/*}"
+DIR_WRK="$DIR_HOME/wrk"
+DIR_LOG="$DIR_HOME/log"
+DIR_TMP="$DIR_HOME/tmp"
+DIR_CFG="$DIR_HOME/cfg"
+FIC_LOG="$DIR_LOG/purges_$HORODATAGE.log"
+#LIST_FIC : Liste des fichiers à supprimer
+LIST_FIC="$DIR_WRK/purges_$HORODATAGE.lst"
+
+#Ouverture de LOG
+#exec 1> $FIC_LOG 2>&1
+
+echo "initPurges [INFO] : 0. Debut programme $0"
+# Check de parametrage passé au programme
+# si rien passe en parametrage dans le programme principale
+if [ $# -eq 0 ]
+then echo "initPurges [INFO] : Aucun nom de fichier de configuration passe en paramètre"
+     echo "initPurges [INFO] : FIC_PARAM=$DIR_CFG/purges.cfg (Valeur par defaut)"
+     FIC_PARAM="$DIR_CFG/purges.cfg"
+#Si un valeur a été passé en paramètre dans le programme principale
+else FIC_PARAM=$1
+     echo "initPurges [INFO] : FIC_PARAM=$1"
+fi
+
+# Creation des environnement de travail pour le script de purges ( Premier fois)
+mkdir -p $DIR_WRK
+mkdir -p $DIR_LOG
+mkdir -p $DIR_TMP
+
+if ! test -s "$FIC_PARAM"
+then    echo "initPurges [ERROR] : Fichier $FIC_PARAM vide ou inexistante"
+        exit 1
+fi
+
+touch $LIST_FIC
+echo "initPurges [INFO] : ####################################"
+echo "initPurges [INFO] : 1. Detail des variables du programme"
+echo "initPurges [INFO] : ####################################"
+echo "initPurges [INFO] :"
+echo "initPurges [INFO] : HORODATAGE = $HORODATAGE"
+echo "initPurges [INFO] : DIR_HOME   = $DIR_HOME"
+echo "initPurges [INFO] : DIR_SHL    = $DIR_SHL"
+echo "initPurges [INFO] : DIR_WRK    = $DIR_WRK"
+echo "initPurges [INFO] : DIR_LOG    = $DIR_LOG"
+echo "initPurges [INFO] : DIR_TMP    = $DIR_TMP"
+echo "initPurges [INFO] : DIR_CFG    = $DIR_CFG"
+echo "initPurges [INFO] : LIST_FIC   = $LIST_FIC"
+echo "initPurges [INFO] :"
+echo "initPurges [INFO] :"
+echo "initPurges [INFO] : Fin de initPurges"
---
+++ shl/purges.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#On ajoute la configuration initiale
+source initPurges.sh
+
+# on boucle sur le fichier $FIC_PARAM
+while read ligne ; do
+    # on saute les lignes vides ou commentées
+    echo "$ligne" | grep -E "^$" > /dev/null && continue
+    echo "$ligne" | grep -E "^#" > /dev/null && continue
+
+    # Pour chaque consigne on parse afin d'obtenir les infos sur les champs
+
+    REP=$( eval echo  $(echo "$ligne" | cut -d";" -f1))
+    FIC_PATTERN=$(echo "$ligne" | cut -d";" -f2)
+    RETENTION=$(echo "$ligne" | cut -d";" -f3)
+    PROFONDEUR=$(echo "$ligne" | cut -d";" -f4)
+    if [ "x$PROFONDEUR" = "x" ]
+    then
+       PROFONDEUR=1
+    fi
+
+    #Check de Repertoire
+    if test ! -d "$REP" -o -z "$REP"; then
+        echo "[WARNING] $REP n'existe pas. Merci de corriger $FIC_PARAM"
+        continue
+    fi
+
+    # Affichage de la configuration pour la ligne de paramétrage
+    echo "[INFO] Purge de REP=$REP FIC_PATTERN=$FIC_PATTERN RETENTION=$RETENTION PROFONDEUR=$PROFONDEUR"
+
+
+    # on purge
+    # on peut tomber sur une erreur si la liste de fichier à supprimer est trop grande à cause du ls
+    # dans ce cas, supprimer ceci de la commande find : -exec ls -1td "{}" +
+    find $REP -name "$FIC_PATTERN" -mtime "+$RETENTION" -maxdepth "$PROFONDEUR" -exec ls -1td "{}" + | while read findLine; do
+        #Obtenir la taille du fichier qu'on va supprimer
+        taille=$(wc -c $findLine | awk '{print $1}')
+        # On supprime le fichier
+        echo "Fichier a supprimer : $findLine"
+	#if  rm -f $findLine
+        #then   printf  "%-150s%15d%10s\n" $findLine $taille SUPPRIME >> $LIST_FIC
+        #else   printf  "%-150s%15d%10s\n" $findLine $taille ERROR >> $LIST_FIC
+        #fi
+    done
+done < "$FIC_PARAM"
+echo "Compte rendu des fichiers purgés : cat $LIST_FIC "
+cat $LIST_FIC
